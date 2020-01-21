b7b62270f38e4965afdf052d7679ae112285b08d
Louisa <louisa.chikar@gmail.com>
2020-01-21 17:32:58 +0100

    Camera Player Maze

---
+++ RotInVR/Assets/Scenes/Scene1/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    private Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector3(0, 1, 0);
+        transform.position = player.position + offset;
+    }
+}
