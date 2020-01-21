c6c458641a439c6339a76c45c1201920219a2301
zouuh <zoedur@gmail.com>
2020-01-21 16:27:07 +0100

    Add infinityScene

---
+++ RotInVR/Assets/Scenes/Scene3/Scripts/alphaChangeX.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class alphaChangeX : MonoBehaviour {
+
+    private Renderer wallRenderer;
+	public float alpha = 0f;
+	public Transform player;
+	private double dist;
+
+    // Start is called before the first frame update
+    void Start() {
+        wallRenderer = GetComponent<Renderer>();
+    	wallRenderer.material.color = new Color(1f, 1f, 1f, alpha);
+    
+    }
+
+    // Update is called once per frame
+    void Update() {
+    	dist = Math.Sqrt(Math.Pow(player.position.x - transform.position.x, 2));
+		if(dist<4) {
+	    	alpha = (float) (0.75 - dist/5);
+    	}
+    	else {
+    		alpha = 0f;
+    	}
+    	wallRenderer.material.color = new Color(1f, 1f, 1f, alpha);
+    }
+}
---
+++ RotInVR/Assets/Scenes/Scene3/Scripts/starGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class starGenerator : MonoBehaviour {
+
+    public GameObject star;
+    public float respawnTime = 0.2f;
+    private Vector3 position; 
+    /*private meshRenderer starRenderer;*/
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(starWave());   
+    }
+
+    float getRandom(float min, float max) {
+        float rand = Random.Range(-max, max);
+        if(-min < rand && rand < min) {
+            if(rand > 0) {
+                rand = Random.Range(min, max);
+            }
+            else {
+                rand = Random.Range(-max, -min);
+            }
+        }
+        return rand;
+    }
+
+    private void spawnStars() {
+
+        position = new Vector3(getRandom(10.0f, 200.0f), getRandom(10.0f, 200.0f), getRandom(10.0f, 200.0f));
+        //position = new Vector3(posX, posY, posZ);
+        GameObject newStar = Instantiate(star, position, Quaternion.identity);
+        /*starRenderer = GetComponent<meshRenderer>();
+        starRenderer.material.color = new Color(Random.Range(0, 255), Random.Range(0, 255), Random.Range(0, 255), 1.0f);*/
+    }
+
+    IEnumerator<WaitForSeconds> starWave() {
+        while(true) {
+            yield return new WaitForSeconds(respawnTime);
+            for(int i=0; i<10; i++) {
+                spawnStars();
+            }
+        }
+    }
+}
