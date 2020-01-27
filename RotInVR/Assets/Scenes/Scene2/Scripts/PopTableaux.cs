618e689d9632bf354a4009146018da87cf68d11c
BenseVR <BenseVR@gmail.com>
2020-01-27 11:03:02 +0100

    Random Vertical perfect

--- RotInVR/Assets/Scenes/Scene2/Scripts/PopTableaux.cs
+++ RotInVR/Assets/Scenes/Scene2/Scripts/PopTableaux.cs
@@ -5,55 +5,69 @@ public class PopTableaux : MonoBehaviour {
 
 // Init others functions
 
-public static GameObject oeuvre1;
-public static GameObject oeuvre2;
-public static GameObject oeuvre3;
-public static GameObject oeuvre4;
+public GameObject oeuvre1;
+public GameObject oeuvre2;
+public GameObject oeuvre3;
+public GameObject oeuvre4;
 
-public static GameObject[] mesOeuvres = { oeuvre1, oeuvre2, oeuvre3, oeuvre4 };
+private GameObject[] mesOeuvres;
 
 
 
     public void initMur1(){
-    for (int i=21; i<87; i+2){
-           int o = Random.Range(0, mesOeuvres.Length-1);
-           GameObject monOeuvre = (GameObject)Instantiate(mesOeuvres[o], new Vector3(-2.8f, i, 5.4f), Quaternion.Euler(0, 180, 0));// To do
+    for (int i=0; i<70; i++){
+            if (i % 2 == 1)
+            {
+                int o = Random.Range(0, mesOeuvres.Length - 1);
+                GameObject monOeuvre = (GameObject)Instantiate(mesOeuvres[o], new Vector3(-1.43f, i, 0), Quaternion.Euler(0, 0, 0));
+            }
+
         }
     }
 
 public void initMur2(){
-        for (int i = 21; i < 87; i + 2){
-            int o = Random.Range(0, mesOeuvres.Length-1);
-            GameObject monOeuvre = (GameObject)Instantiate(mesOeuvres[o], new Vector3(-1.44f, i, 6.8f), Quaternion.Euler(0, -90, 0)); // To do
+        for (int i = 0; i < 70; i++){
+            if (i % 2 == 1)
+            {
+                int o = Random.Range(0, mesOeuvres.Length - 1);
+                GameObject monOeuvre = (GameObject)Instantiate(mesOeuvres[o], new Vector3(0, i, 1.39f), Quaternion.Euler(0, 90, 0));
+            }
         }
     }
 
 public void initMur3(){
-        for (int i = 21; i < 87; i + 2)
+        for (int i = 0; i < 70; i++)
         {
-            int o = Random.Range(0, mesOeuvres.Length-1);
-            GameObject monOeuvre = (GameObject)Instantiate(mesOeuvres[o], new Vector3(0f, i, 5.4f), Quaternion.Euler(0, 0, 0));// To do
+            if (i % 2 == 1)
+            {
+                int o = Random.Range(0, mesOeuvres.Length - 1);
+                GameObject monOeuvre = (GameObject)Instantiate(mesOeuvres[o], new Vector3(1.43f, i, 0), Quaternion.Euler(0, 180, 0));
+            }
         }
     }
 
 public void initMur4(){
-        for (int i = 21; i < 87; i + 2)
+        for (int i = 0; i < 70; i++)
         {
-            int o = Random.Range(0, mesOeuvres.Length-1);
-            GameObject monOeuvre = (GameObject)Instantiate(mesOeuvres[o], new Vector3(-1.44f,i, 4f), Quaternion.Euler(0, 90, 0));// To do
+            if (i % 2 == 1)
+            {
+                int o = Random.Range(0, mesOeuvres.Length - 1);
+            GameObject monOeuvre = (GameObject)Instantiate(mesOeuvres[o], new Vector3(0, i, -1.39f), Quaternion.Euler(0, -90, 0));
+            }
         }
     }
 
 public void Shuffle()
     {
-
+    //initMur1();
+    //initMur2();
+    //initMur3();
+    //initMur4();
     }
 
 void Start(){
 
-mesOeuvres = GameObject.FindGameObjectsWithTag("Tableau");
-
-
+       mesOeuvres = new GameObject[4] { oeuvre1, oeuvre2, oeuvre3, oeuvre4 };
 initMur1();
 initMur2();
 initMur3();
